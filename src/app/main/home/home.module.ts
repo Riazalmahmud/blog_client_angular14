96c597e6b09ea4b85b241ba4ae321ff1480ee679
Riazalmahmud <riazalmahmud041@gmail.com>
2022-10-16 21:03:31 +0600

    home

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { SliderComponent } from './slider/slider.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SliderComponent, ArticleComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
+  providers: [],
 })
 export class HomeModule {}
